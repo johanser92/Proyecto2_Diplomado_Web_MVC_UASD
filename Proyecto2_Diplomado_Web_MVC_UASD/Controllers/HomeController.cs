789960a8ddf2f23dbbf2b42f89cbc181e2b033c8
johanser92 <40702293+johanser92@users.noreply.github.com>
2019-11-01 00:02:53 -0400

    Practica 2

---
+++ Proyecto2_Diplomado_Web_MVC_UASD/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto2_Diplomado_Web_MVC_UASD.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Fundadores()
+        {
+            return View();
+        }
+
+        public ActionResult Campeonatos()
+        {
+            return View();
+        }
+    }
+}
